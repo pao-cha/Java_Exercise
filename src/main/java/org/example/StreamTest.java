2fbed5f2c62303d1c0626cb395efbab3200d0020
mingyu.guo <mingyu.guo@xquant.com>
2024-10-21 16:57:31 +0800

    feat: 还原

--- src/main/java/org/example/StreamTest.java
+++ src/main/java/org/example/StreamTest.java
@@ -1,48 +1,23 @@
 package org.example;
 
+import java.time.LocalTime;
+import java.util.*;
+
 /**
- * <p>Title: xCRMS </p>
- * Description: 通用可逆加解密工具 <br>
- * Copyright: CorpRights xQuant.com<br>
- * Company: xQuant.com<br>
- *
- * @author yujj
- * @version 1.1.1.RELEASE
- * @date 2019-04-12
+ * @author mingyu, guo
+ * @date 2024-06-03 10:27
  */
-public final class Base64EncodeUtil {
 
-    public static final String ENCRYPT_FACTOR = "xQuant_xCRMS";
-
+public class StreamTest {
     public static void main(String[] args) {
 
-
-        final String text = "674643";
-
-        System.out.println(String.format("Plain Text:%s，Cipher Text:%s", text, encode(text)));
+        String a = "1122";
+        String b = getString(a);
+        System.out.println(a);
+        System.out.println(b);
     }
-
-    /**
-     * 加密（使用默认盐）
-     *
-     * @param text
-     * @return
-     */
-    public static String encode(String text) {
-
-        return encode(text, ENCRYPT_FACTOR);
-    }
-
-    /**
-     * 加密（使用指定盐）
-     *
-     * @param text
-     * @param key
-     * @return
-     */
-    public static String encode(String text, String key) {
-
-        byte[] bs = RC4.encodeRC4_byte(text, key);
-        return Base64.encode(bs);
+    private static String getString(String a) {
+        a = "23";
+        return a;
     }
 }
